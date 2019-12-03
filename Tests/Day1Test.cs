0cab18f65ce9cf0fb8661edfec1529736f59d19a
Jchurch <Jchurch@miradorfin.com>
2019-12-03 14:38:04 -0800

    Day1: Problems 1 and 2 tests

---
+++ Tests/Day1Test.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+
+namespace AdventOfCodeDay1
+
+{
+	[TestFixture]
+	public class FuelCalculatorTest
+	{
+
+		[Test]
+		[TestCase(12, 2)]
+		[TestCase(14, 2)]
+		[TestCase(1969, 654)]
+		[TestCase(100756, 33583)]
+		public void Test_GetFuel(int input, int expectedResult) =>
+			Assert.AreEqual(expectedResult, FuelCalculator.GetFuel(input));
+
+        [Test]
+        [TestCase(12, 2)]
+        [TestCase(14, 2)]
+        [TestCase(1969, 966)]
+        [TestCase(100756, 50346)]
+        public void Test_GetCompoundedFuel(int input, int expectedResult) =>
+            Assert.AreEqual(expectedResult, FuelCalculator.GetCompoundedFuel(input));
+
+    }
+}
