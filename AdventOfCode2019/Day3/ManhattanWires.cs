0812a4c1a7c8b5f1f92246a2d0cee81e7dcbc4db
Jchurch <Jchurch@miradorfin.com>
2019-12-03 23:07:44 -0800

    Day3: Setup day 3 test suite

---
+++ AdventOfCode2019/Day3/ManhattanWires.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AdventOfCode2019.Day3
+{
+    public class ManhattanWires
+    {
+        public static int GetDistance(String wireAInstructions, String wireBInstructions )
+        {
+            return 0;
+        }
+    }
+}
