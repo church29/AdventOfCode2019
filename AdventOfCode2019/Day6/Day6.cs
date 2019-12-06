47bc9ec70c888ef3c0439ff6321a7f95e885592b
Jchurch <Jchurch@miradorfin.com>
2019-12-05 21:24:58 -0800

    Day6: get first test passing

--- AdventOfCode2019/Day6/Day6.cs
+++ AdventOfCode2019/Day6/Day6.cs
@@ -12,11 +12,43 @@ namespace AdventOfCode2019.Day6 {
            
 
             Console.WriteLine("Day 6: Problem 1: ");
+            CalculateNumOrbits(lines.ToList());
           
         }
 
         public static int CalculateNumOrbits(List<string> orbitList) {
-            return 0;
+            var orbitMap = GetOrbitMap(orbitList);
+            var numOrbits = 0;
+            foreach(var planet in orbitMap.Keys) {
+                numOrbits += CalculateNumOrbitsForPlanet(orbitMap, planet);
+
+            }
+
+            return numOrbits;
         }
+
+        public static Dictionary<string, string> GetOrbitMap(List<string> orbitList) {
+            var orbitMap = new Dictionary<string, string>();
+            foreach(var orbit in orbitList) {
+                var orbits = orbit.Split(")");
+                orbitMap.Add(orbits[1], orbits[0]);
+            }
+
+            return orbitMap;
+        }
+
+        public static int CalculateNumOrbitsForPlanet(Dictionary<string, string> orbitMap, string planet) {
+            if (planet == "COM") {
+                return 0;
+            }
+
+            string orbitingAround;
+            if (orbitMap.TryGetValue(planet, out orbitingAround)) {
+                return 1 + CalculateNumOrbitsForPlanet(orbitMap, orbitingAround);
+            }
+
+            return 0;
+
+                    }
     }
 }
