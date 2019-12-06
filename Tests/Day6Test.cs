432f9e88e0821bfe2405ac2758d87c062542fac0
Jchurch <Jchurch@miradorfin.com>
2019-12-05 21:27:47 -0800

    Day6: add large test case

--- Tests/Day6Test.cs
+++ Tests/Day6Test.cs
@@ -12,6 +12,7 @@ namespace AdventOfCode2019.Day6 {
         [TestCase("a)b", 1)]
         [TestCase("a)b,c)d", 2)]
         [TestCase("a)b,c)d,b)e", 4)]
+        [TestCase("COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L", 42)]
         public void Test_CalculateNumOrbits(string input, int expectedResult) =>
             Assert.AreEqual(
                 expectedResult,
