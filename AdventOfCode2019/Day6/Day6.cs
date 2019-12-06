69f868abe49121b4f92b03a39ab90f45815e7f77
Jchurch <Jchurch@miradorfin.com>
2019-12-05 21:10:01 -0800

    Day6: setup day 6

---
+++ AdventOfCode2019/Day6/Day6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Environment;
+
+
+namespace AdventOfCode2019.Day6 {
+    public class OrbitalSanta {
+        public static void Day6Problem() {
+            var filePath = CurrentDirectory.ToString() + "/Day5/resources/input.txt";
+            var lines = System.IO.File.ReadAllLines(filePath);
+           
+
+            Console.WriteLine("Day 6: Problem 1: ");
+          
+        }
+
+        public static int CalculateNumOrbits(List<string> orbitList) {
+            return 0;
+        }
+    }
+}
---
+++ Tests/Day6Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode2019.Day2;
+using NUnit.Framework;
+
+namespace AdventOfCode2019.Day6 {
+
+    [TestFixture]
+    public class OrbitalSantaTest {
+
+        [Test]
+        [TestCase("a)b", 1)]
+        public void Test_CalculateNumOrbits(string input, int expectedResult) =>
+            Assert.AreEqual(
+                expectedResult,
+                OrbitalSanta.CalculateNumOrbits(input.Split(",").ToList()));
+
+    }
+}
