0930c16cfa999e1a8bcb2b7e6e7814fe14ddd3c8
Jchurch <Jchurch@miradorfin.com>
2019-12-05 21:26:07 -0800

    Day6: add more tests

--- Tests/Day6Test.cs
+++ Tests/Day6Test.cs
@@ -10,6 +10,8 @@ namespace AdventOfCode2019.Day6 {
 
         [Test]
         [TestCase("a)b", 1)]
+        [TestCase("a)b,c)d", 2)]
+        [TestCase("a)b,c)d,b)e", 4)]
         public void Test_CalculateNumOrbits(string input, int expectedResult) =>
             Assert.AreEqual(
                 expectedResult,
