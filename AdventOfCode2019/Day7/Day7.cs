fc4e2c9d2dc95ca9802783ad2c1a0612faf0587f
Jchurch <Jchurch@miradorfin.com>
2019-12-07 15:52:07 -0800

    Day7: Generate permutations

--- AdventOfCode2019/Day7/Day7.cs
+++ AdventOfCode2019/Day7/Day7.cs
@@ -10,16 +10,16 @@ namespace AdventOfCode2019.Day7 {
             var filePath = CurrentDirectory.ToString() + "/Day7/resources/input.txt";
             var lines = System.IO.File.ReadAllLines(filePath);
             var intcode = lines.First().Split(",").Select(int.Parse).ToList();
-
-            var permutations = System.IO.File.ReadAllLines(CurrentDirectory.ToString() + "/Day7/resources/permutations.txt");
-
-
+            
+            var permutations = GetPermutations(new List<int>() { 0,1,2,3,4}, 5);
+            // var permutations = System.IO.File.ReadAllLines(CurrentDirectory.ToString() + "/Day7/resources/permutations.txt");
+                        
             var maxes = new List<int>();
             var intCodes = new Dictionary<int, List<int>>();
             foreach (var permutation in permutations) {
                 maxes.Add(getMaxThrusterAtPhaseSetting(
                     new List<int>(intcode),
-                    permutation.Select(Char.ToString).Select(int.Parse).ToList(),
+                    permutation,
                     new List<int>() { 0, 0, 0, 0, 0 },
                     intCodes
                     ));
@@ -27,32 +27,34 @@ namespace AdventOfCode2019.Day7 {
 
             Console.WriteLine("Day 7: Problem 1: " + maxes.Max());
 
-            permutations = System.IO.File.ReadAllLines(CurrentDirectory.ToString() + "/Day7/resources/problem2.txt");
-
-
+            permutations = GetPermutations(new List<int>() { 5, 6, 7, 8, 9 }, 5);
+            // permutations = System.IO.File.ReadAllLines(CurrentDirectory.ToString() + "/Day7/resources/problem2.txt");
+            
             maxes = new List<int>();
             intCodes = new Dictionary<int, List<int>>();
             foreach (var permutation in permutations) {
                 maxes.Add(getMaxThrusterAtPhaseSetting(
                     new List<int>(intcode),
-                    permutation.Select(Char.ToString).Select(int.Parse).ToList(),
+                    permutation,
                     new List<int>() { 0, 0, 0, 0, 0 },
                     intCodes
                     ));
             }
 
             Console.WriteLine("Day 7: Problem 2: " + maxes.Max());
-
         }
 
-        private static int getMaxThrusterAtPhaseSetting(List<int> list1, List<int> list2) {
-            throw new NotImplementedException();
+        public static List<List<int>> GetPermutations(List<int> list, int length) {
+            if (length == 1) {
+                return list.Select(p => new List<int>() { p }).ToList();
+            }
+            return GetPermutations(list, length - 1)
+                .SelectMany(
+                    permutation => list.Where(phase => !permutation.Contains(phase)),
+                    (t1, t2) => t1.Concat(new List<int>(){ t2 }).ToList()
+                ).ToList();
         }
-
-        private class PhaseState {
-
-        }
-
+               
         public static int getMaxThrusterAtPhaseSetting(
             List<int> intCode,
             List<int> phaseSetting,
@@ -62,7 +64,7 @@ namespace AdventOfCode2019.Day7 {
             Boolean initialRun = true
 
         ) {
-           
+
             foreach (var phase in phaseSetting) {
 
                 var inputs = new List<int>();
@@ -77,11 +79,11 @@ namespace AdventOfCode2019.Day7 {
                 if (intCodes.TryGetValue(phase, out phaseCode)) {
 
                     results = processIntCode(phaseCode, startingPositions[phaseSetting.IndexOf(phase)], inputs);
-                    
+
                 } else {
                     phaseCode = new List<int>(intCode);
                     results = processIntCode(phaseCode, startingPositions[phaseSetting.IndexOf(phase)], inputs);
-                   
+
                 }
 
                 intCodes[phase] = phaseCode;
@@ -92,11 +94,9 @@ namespace AdventOfCode2019.Day7 {
 
                 input = results.First();
                 startingPositions[phaseSetting.IndexOf(phase)] = results.Last();
-                                
+
             }
-
-
-
+                       
             return getMaxThrusterAtPhaseSetting(
                 new List<int>(intCode),
                 phaseSetting,
@@ -105,27 +105,15 @@ namespace AdventOfCode2019.Day7 {
                 input,
                 false
              );
-
         }
-
-
+        
         public static List<int> processIntCode(List<int> intCode, int startingPosition, List<int> inputs) {
             var instructionCode = intCode[startingPosition];
             var opCode = instructionCode % 100;
             var param1Mode = instructionCode.ToString().Length >= 3 ? int.Parse(instructionCode.ToString()[^3].ToString()) : 0;
             var param2Mode = instructionCode.ToString().Length >= 4 ? int.Parse(instructionCode.ToString()[^4].ToString()) : 0;
             var param3Mode = instructionCode.ToString().Length >= 5 ? int.Parse(instructionCode.ToString()[^5].ToString()) : 0;
-
-            /*
-            Console.WriteLine(
-                "instructionCode: " + instructionCode +
-                " opCode " + opCode +
-                " param1Mode: " + param1Mode +
-                " param2Mode: " + param2Mode +
-                " param3Mode: " + param3Mode
-            );
-            */
-
+             
             if (opCode == 99) {
                 var results = new List<int>();
                 return results;
@@ -137,10 +125,8 @@ namespace AdventOfCode2019.Day7 {
             if (opCode == 3) {
                 intCode[value1Address] = inputs.First();
                 inputs = inputs.Skip(1).ToList();
-                Console.WriteLine("INPUT: opCode 3 Input " + inputs + " at : " + value1Address + " instructionCode: " + instructionCode + "\n");
                 return processIntCode(intCode, startingPosition + getIncrementFromOpCode(opCode), inputs);
             } else if (opCode == 4) {
-                Console.WriteLine("OUTPUT: opCode 4 Output at value1Address: " + value1 + " instructionCode: " + instructionCode + "\n");
                 var results = new List<int>() { value1, (startingPosition + getIncrementFromOpCode(opCode)) };
                 return results;
 
@@ -160,16 +146,6 @@ namespace AdventOfCode2019.Day7 {
             var resultAddress = intCode[startingPosition + 3];
             var result = processOperation(opCode, value1, value2);
             intCode[resultAddress] = result;
-
-            /*
-            Console.WriteLine(
-                "value1Address: " + value1Address +
-                " value1: " + value1 +
-                " value2Address: " + value2Address +
-                " value2: " + value2 +
-                " resultAddress: " + resultAddress +
-                " result: " + result);
-            */
 
             return processIntCode(intCode, startingPosition + getIncrementFromOpCode(opCode), inputs);
         }
@@ -204,9 +180,6 @@ namespace AdventOfCode2019.Day7 {
 
 
             return value1 * value2;
-
-
-
 
         }
     }
