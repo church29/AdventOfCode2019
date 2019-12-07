bff5371efef290d5ae7e4da984d47de7922be9b7
Jchurch <Jchurch@miradorfin.com>
2019-12-06 20:49:06 -0800

    Day7: Setup day7

---
+++ AdventOfCode2019/Day7/Day7.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Environment;
+
+namespace AdventOfCode2019.Day7 {
+    public class InterestingDay7Name {
+        public static void Day7() {
+            var filePath = CurrentDirectory.ToString() + "/Day7/resources/input.txt";
+            var lines = System.IO.File.ReadAllLines(filePath);
+        }
+    }
+}
---
+++ Tests/Day7Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace AdventOfCode2019.Day7 {
+
+    [TestFixture]
+    public class Day7Test {
+
+        [Test]
+        [TestCase("a)b", 1)]
+        [TestCase("a)b,c)d", 2)]
+        [TestCase("a)b,c)d,b)e", 4)]
+        [TestCase("COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L", 42)]
+        public void Test_CalculateNumOrbits(string input, int expectedResult) =>
+            Assert.AreEqual(
+                expectedResult,
+                0
+                );
+
+    }
+}
