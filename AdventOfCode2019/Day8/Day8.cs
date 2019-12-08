f21b4f596af87f23e991ff696d09f2af57dc3d2a
Jchurch <Jchurch@miradorfin.com>
2019-12-07 21:15:09 -0800

    Day8: Setup day 8

---
+++ AdventOfCode2019/Day8/Day8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Environment;
+
+namespace AdventOfCode2019.Day8 {
+
+
+
+    public class MultiInputIntCodeComputer {
+        public static void Day7() {
+            var filePath = CurrentDirectory.ToString() + "/Day8/resources/input.txt";
+            var lines = System.IO.File.ReadAllLines(filePath);
+          
+
+            Console.WriteLine("Day 8: Problem 1: " + 0);
+
+            
+
+            Console.WriteLine("Day 8: Problem 2: " + 0);
+        }
+    }
+}
