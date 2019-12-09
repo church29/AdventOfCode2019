3c3eed494c740df9584432b903098a75c9df46ed
Jchurch <Jchurch@miradorfin.com>
2019-12-08 22:47:23 -0800

    Day9: Solve problem 1

--- AdventOfCode2019/Day9/Day9.cs
+++ AdventOfCode2019/Day9/Day9.cs
@@ -3,10 +3,135 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using static System.Environment;
+using static System.Linq.Enumerable;
 
 namespace AdventOfCode2019.Day9 {
-    public class Elf9 {
+    public class BoostIntCodeComputer {
         public static void Day9() {
+            var filePath = CurrentDirectory.ToString() + "/Day9/resources/input.txt";
+            var lines = System.IO.File.ReadAllLines(filePath);
+            var intCode = lines.First().Split(",").Select(long.Parse).ToList();
+
+            var output = processIntCode(intCode, 0, new List<long>() { 1 }, new List<long>(), 0);
+            Console.WriteLine("Day 9: Problem 1: " + String.Join(",", output));
+
+
+
+            Console.WriteLine("Day 9: Problem 2: " + 0);
+        }
+
+       
+
+       
+        public static List<long> processIntCode(List<long> intCode, int startingPosition, List<long> inputs, List<long> output, long relativeBase = 0) {
+            if (intCode.Count < 4096) {
+                intCode.AddRange(Repeat(0L, 4096).ToList());
+                             
+            }
+            var instructionCode = intCode[startingPosition];
+            var opCode = instructionCode % 100;
+            var param1Mode = instructionCode.ToString().Length >= 3 ? int.Parse(instructionCode.ToString()[^3].ToString()) : 0;
+            var param2Mode = instructionCode.ToString().Length >= 4 ? int.Parse(instructionCode.ToString()[^4].ToString()) : 0;
+            var param3Mode = instructionCode.ToString().Length >= 5 ? int.Parse(instructionCode.ToString()[^5].ToString()) : 0;
+
+            if (opCode == 99) {
+                return output;
+            }
+
+            var value1Address = intCode[startingPosition + 1];
+            var value1 = GetValueAtPosition(intCode, startingPosition + 1, param1Mode, relativeBase);
+
+            if (opCode == 3) {
+                if (param1Mode == 2) {
+                    intCode[(int)(value1Address + relativeBase)] = inputs.First();
+                } else {
+                    intCode[(int)value1Address] = inputs.First();
+                }
+               
+                inputs = inputs.Skip(1).ToList();
+                return processIntCode(intCode, startingPosition + getIncrementFromOpCode(opCode), inputs, output, relativeBase);
+            }
+            if (opCode == 4) {
+                Console.WriteLine("OUTPUT: opCode 4 Output at value1Address: " + value1 + " instructionCode: " + instructionCode + "\n");
+                output.Add(value1);
+                return processIntCode(intCode, startingPosition + getIncrementFromOpCode(opCode), inputs, output, relativeBase);
+
+            }
+            if (opCode == 9) {
+                Console.WriteLine("new relative base: " + (relativeBase + value1));
+                return processIntCode(intCode, startingPosition + getIncrementFromOpCode(opCode), inputs, output, relativeBase + value1);
+            }
+
+            var value2 = GetValueAtPosition(intCode, startingPosition + 2, param2Mode, relativeBase);
+
+            if (opCode == 5) {
+                return processIntCode(intCode, (int)(value1 != 0 ? value2 : (startingPosition + getIncrementFromOpCode(opCode))), inputs, output, relativeBase);
+            }
+
+            if (opCode == 6) {
+                return processIntCode(intCode, (int)(value1 == 0 ? value2 : (startingPosition + getIncrementFromOpCode(opCode))), inputs, output, relativeBase);
+            }
+
+
+
+            var resultAddress = intCode[startingPosition + 3] + (param3Mode == 2 ? relativeBase : 0);
+
+            var result = processOperation(opCode, value1, value2);
+            intCode[(int)resultAddress] = result;
+
+            return processIntCode(intCode, startingPosition + getIncrementFromOpCode(opCode), inputs, output, relativeBase);
+        }
+
+
+        public static long GetValueAtPosition(List<long> intCode, int paramPosition, int paramMode, long relativeBase) {
+
+            var valueAddress = intCode[paramPosition];
+            switch (paramMode) {
+                case 0:
+                return intCode[(int)valueAddress];
+                case 1:
+                return valueAddress;
+                default:
+
+                return intCode[(int)(valueAddress + relativeBase)];
+
+
+            }
+        }
+
+        static int getIncrementFromOpCode(long opCode) {
+            switch (opCode) {
+                case 3:
+                case 4:
+                case 9:
+                return 2;
+
+                case 5:
+                case 6:
+                return 3;
+                default:
+                return 4;
+            }
+        }
+
+        static long processOperation(long opCode, long value1, long value2) {
+
+            if (opCode == 1) {
+                return value1 + value2;
+            }
+
+            if (opCode == 7) {
+                return value1 < value2 ? 1 : 0;
+            }
+
+            if (opCode == 8) {
+                return value1 == value2 ? 1 : 0;
+            }
+
+
+            return value1 * value2;
+
         }
     }
 }
+
--- Tests/Day9Test.cs
+++ Tests/Day9Test.cs
@@ -10,19 +10,64 @@ using NUnit.Framework;
 namespace AdventOfCode2019.Day9 {
 
     [TestFixture]
-    public class Day9Test {
+    public class IntCodeComputerTest {
 
+        /*
         [Test]
-        [TestCase("99,0,0", "4,3,2,1,0", 0)]
-        [TestCase("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", "4,3,2,1,0", 43210)]
-        [TestCase("3,23,3,24,1002,24,10,24,1002,23,-1,23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0", "0,1,2,3,4", 54321)]
-        public void Test_Thrusters(string input, string phaseCode, int expectedResult) =>
+        [TestCase("1,0,0,0,99", "2,0,0,0,99")]
+        [TestCase("2,3,0,3,99", "2,3,0,6,99")]
+        [TestCase("2,4,4,5,99,0", "2,4,4,5,99,9801")]
+        [TestCase("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
+        public void Test_ProcessIntCode(string input, string expectedResult) =>
             Assert.AreEqual(
                 expectedResult,
-                0
-                );
+                String.Join(",", BoostIntCodeComputer.processIntCode(input.Split(",").Select(int.Parse).ToList(), 0, new List<int>(), 0).ToArray()));
+
+    */
+        
+        [Test]
 
        
+
+       
+        [TestCase(
+            "104,1125899906842624,99",
+            1125899906842624
+        )]
+        public void Test_ProcessIntCode2(string input, long expectedResult) =>
+            Assert.AreEqual(
+                expectedResult,
+                BoostIntCodeComputer.processIntCode(input.Split(",").Select(long.Parse).ToList(), 0, new List<long>(), new List<long>(), 0).First());
+        [Test]
+        [TestCase(
+            "1102,34915192,34915192,7,4,7,99,0",
+            16
+        )]
+        public void Test_ProcessIntCodeLength(string input, long expectedResult) =>
+           Assert.AreEqual(
+               expectedResult,
+               BoostIntCodeComputer.processIntCode(input.Split(",").Select(long.Parse).ToList(), 0, new List<long>(), new List<long>(), 0).First().ToString().Length);
+
+        [Test]
+        [TestCase(
+            "109,1,204,-1,99,0,0",
+            "109"
+            )]
+        [TestCase(
+            "109,1,204,-1,109,2,204,-1,99,0,0",
+            "109,204"
+            )]
+        [TestCase(
+            "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99",
+            "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+
+        )]
+        public void Test_ProcessIntCodeOutput(string input, string expectedResult) =>
+           Assert.AreEqual(
+               expectedResult,
+               String.Join(",", BoostIntCodeComputer.processIntCode(input.Split(",").Select(long.Parse).ToList(), 0, new List<long>(), new List<long>(), 0)));
+
     }
+
 }
 
